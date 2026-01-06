7ef54c92e99382282ee84344a19f32ab78a94017
James-Cole-work <james.cole1@open.ac.uk>
2026-01-06 15:39:22 +0000

    Update PopupForms.js

--- js_scripts2/PopupForms.js
+++ js_scripts2/PopupForms.js
@@ -517,8 +517,8 @@ function cancelPopup() { //closes form when done
 
 
 document.getElementById('openDaySelector').addEventListener('click', () => {
-    const width = screen.availWidth * 0.6;   // 50% of available screen width
-    const height = screen.availHeight * 0.6; // 50% of available screen height
+    const width = screen.availWidth * 0.4;   // 50% of available screen width
+    const height = screen.availHeight * 0.4; // 50% of available screen height
 
     // Calculate coordinates to center the popup
     const right = (screen.availWidth - width) / 2;
@@ -664,4 +664,5 @@ document.getElementById('openDaySelector').addEventListener('click', () => {
           
           });
 });
+
 
